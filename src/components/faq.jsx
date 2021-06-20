4f7e7fd9d2b8eb1f542658b3fc9b2f710aa3d65d
abhi <forgames.farzi@gmail.com>
2021-06-20 11:20:29 +0530

    added all pages

---
+++ src/components/faq.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function faq(){
+    return (
+        <div id="faq" class="powerful-area pt-100">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="section-title">
+                            <h2>FAQs (Frequently Asked Questions</h2>
+                        </div>
+                        <div class="powerful-card">
+
+                            <h3> Q : Why a Sock?</h3>
+                            <p>We believe that you need nothing more than your wardrobe to be able to monitor your health. Moreover, access to the feet allows us to deal with foot ulceration which is one of the most common diabetic disorders and also allows us to give accurate readings for BP and heart rates. And lastly, socks are an integral part of every wardrobe prompting a regular use, in turn, giving you the best results.</p>
+                        </div>
+
+                        <div class="powerful-card">
+
+                            <h3>Q : Size of the reader, will it be too obtrusive/visible?</h3>
+                            <p>No not at all, the reader is only 15mm in diameter and the thickness is less than 1mm.</p>
+                        </div>
+
+                        <div class="powerful-card">
+
+                            <h3>Q : Will the socks have padding? How comfortable will it be to wear?</h3>
+                            <p>Yes, since this is a sock design for diabetics, we have ensured that it has comfortable padding to prevent injuries to the foot especially for neuropathic people. The fabric will be bamboo cotton and is extremely breathable.</p>
+                        </div>
+
+                        <div class="powerful-card">
+
+                            <h3>Q : How often should I ideally keep the socks on for an accurate reading?</h3>
+                            <p>A reading can be acquired with only a few minutes of use but for optimal results that predict your health accurately, we recommend regularly using the socks. That is why we ship 6 pairs of socks/box for ease of use.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="powerful-img">
+                            <img src="http://feetwings.com/assets/images/FAQ.svg" alt="Image" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default faq;
---
+++ src/components/special.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+function special(){
+    return (
+        <div id="special" class="easy-step-area ptb-100">
+            <div class="container">
+                <div class="section-title">
+                    <h1>What Makes Us Special ?</h1>
+                </div>
+
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="easy-step-img">
+                            <img src="http://feetwings.com/assets/images/app-img/mobile-app.png" class="step-1" alt="Image" />
+                            <div class="easy-step-shape">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="easy-step-card left-text">
+                            <div class="row align-items-center">
+                                <div class="col-lg-4 col-sm-6">
+                                    <img src="http://feetwings.com/assets/images/icons/Early%20Detection.png" width="110" />
+                                </div>
+                                <div class="col-lg-8 col-sm-6">
+                                    <h5>Early Detection & prevention of Diabetic Complications</h5>
+                                    <p>Find out early signs of risk for severe diabetic complications such as Diabetic Foot Ulcers, Hypertension & Chronic Kidney Disease.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="easy-step-card right-text">
+                            <div class="row align-items-center">
+                                <div class="col-lg-8 col-sm-6">
+                                    <h5>Remote & Continuous Monitoring</h5>
+                                    <p>Real-time and long-term data that helps reduce false positives and helps you make better lifestyle choices from the convenience of your homes.</p>
+                                </div>
+                                <div class="col-lg-4 col-sm-6">
+                                    <img src="http://feetwings.com/assets/images/icons/Remote%20Monitoring.png" width="110" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="easy-step-card left-text">
+                            <div class="row align-items-center">
+                                <div class="col-lg-4 col-sm-6">
+                                    <img src="http://feetwings.com/assets/images/icons/Ease%20of%20access.png" width="110" />
+                                </div>
+                                <div class="col-lg-8 col-sm-6">
+                                    <h5>Ease of Access</h5>
+                                    <p>Our smart yarn is completely machine-washable and dryable, functionally smart, but just as easy to use!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="easy-step-card right-text">
+                            <div class="row align-items-center">
+                                <div class="col-lg-8 col-sm-6">
+                                    <h5>Affordability</h5>
+                                    <p>For the first time ever, continuous, specific and feature-rich diabetic monitoring that is extremely affordable and value-driven with plans starting as low as ₹299/month!</p>
+                                </div>
+                                <div class="col-lg-4 col-sm-6">
+                                        <img src="http://feetwings.com/assets/images/icons/Affordability.png" width="110" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+}
+
+export default special;
