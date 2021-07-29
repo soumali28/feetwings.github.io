909e3c75cb70a3543326e0d52438adbf73c3cf2c
Chetna Gola <chetnagola@gmail.com>
2021-07-29 21:00:36 +0530

    issue9 updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,15 +3,16 @@ import React from "react";
 function footer(){
     return (
         <footer class="footer-area pt-200">
-            <div class="container">
+                <div class="container">
                 </div>
-                <div class="backToTop" align="right">
-                  <button id="myButton"><a href="#home">Back to Top</a></button>
+                <div class="backToTop">
+                <a href="#home">
+                  <i class="fas fa-long-arrow-alt-up"></i>
+                </a>
                 </div>
                 <div class="copyright-text">
                     <p>Copyright @2021 FeetWings. All Rights Reserved </p>
                 </div>
-
         </footer>
     );
 }
