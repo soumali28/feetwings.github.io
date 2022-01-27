1c4e60e404daaddb5450d43f2ba19784a86c93f6
shubham <gshubham9896@gmail.com>
2022-01-27 23:29:54 +0530

    my commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import Footer from "./components/footer";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 ReactDOM.render( <App />,  document.getElementById("root"));
+// my changes
 
 
