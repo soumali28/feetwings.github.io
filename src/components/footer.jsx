206361bf61727c526b488b40bc62a5540d921fba
shubham <gshubham9896@gmail.com>
2022-02-17 00:37:06 +0530

    footer created

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,56 +1,33 @@
 import React from "react";
+import {FaCode} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
+import {FiThumbsUp} from "react-icons/fi";
+import {MdOutlineCopyright} from "react-icons/md";
 
-function footer(){
+
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+
+const Footer = ()=>{
     return (
-        <footer class="footer-area pt-200">
-                <div class="footer-widget">
-                  <div class="logo">
-                    <img src="../assets/logo_web.png" alt="logo" />
-                    <p class="footer-text">Smart Textile Based Healthcare Assistant</p>
-                  </div>
+        <>
+            <div className="d-flex flex-column px-3  align-items-center font-linkk " style={{backgroundColor : "#dadada"}}><br />
 
-                <div class="pages">
-                  <h1 class="title">Pages</h1>
-                    <ul class="footer-text">
-                      <li><a href="#features">Features</a></li>
-                      <li><a href="#work">Work</a></li>
-                      <li><a href="#faq">FAQ</a></li>
-                      <li><a href="#">Career</a></li>
-                    </ul>
-                </div>
+                <br />
 
-                <div class="company">
-                  <h1 class="title">Company</h1>
-                    <ul class="footer-text">
-                      <li>About us</li>
-                      <li>Our Services</li>
-                      <li>Privacy Policy</li>
-                      <li>Terms & Conditions</li>
-                    </ul>
-                </div>
 
-                <div class="social">
-                  <h1 class="title">Follow us</h1>
-                    <ul class="footer-text">
-                      <li><a href="https://www.linkedin.com/company/feetwings-pvt-ltd/" target="_blank">LinkedIn</a></li>
-                      <li><a href="#" target="_blank">Twitter</a></li>
-                      <li><a href="#" target="_blank">Facebook</a></li>
-                      <li><a href="https://www.instagram.com/feet.wings/" target="_blank">Instagram</a></li>
-                    </ul>
+                <div className="d-flex align-items-center gap-4 mb-4">
+                    <div className="d-flex align-items-center gap-4 mr-1" ><FaGithub size={42} className="fs-4" /></div>
+                    <div className="d-flex align-items-center gap-4 mr-1"><FaLinkedin size={42} className="fs-4" /></div>
+                    <div className="d-flex align-items-center gap-4 mr-1"><FaInstagram size={42} className="fs-4" /></div>
                 </div>
-
-                <div class="reach-us">
-                  <h1 class="title">Reach Us</h1>
-                  <p class="footer-text"><a href="mailto:feetwings@gmail.com" target="_blank">feetwings@gmail.com</a></p>
-                </div>
-
-                <div class="backToTop"></div>
-                </div>
-                <div class="copyright-text">
-                    <p>Copyright @2021 FeetWings. All Rights Reserved </p>
-                </div>
-        </footer>
+                
+                <div class="mb-2 " style={{fontSize:"20px"}}>Terms & Conditions  |   Privacy Policy  |   FAQs  |   Career</div>
+                <div style={{fontSize:"20px"}}>Copyright @2021 FeetWings. All Rights Reserved</div><br />
+            </div>
+        </>
     );
 }
 
-export default footer;
+export default Footer;
--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function work() {
     return (
         <div id="work" class="how-we-work service-shape pt-100 pb-70 ">
-            <div class="container hide-on-mobile">
+            <div class="container hide-on-mobile" style={{marginBottom:"80px"}}>
                 <div class="section-title">
                     <h1 class="work-heading-text"></h1>
                 </div>
@@ -45,7 +45,7 @@ function work() {
 
 
 
-            <div class=" container show-on-mobile">
+            <div class=" container show-on-mobile" style={{marginBottom:"80px"}}>
                 <div class="section-title">
                     <h1 class="work-heading-text"></h1>
                 </div>
@@ -109,6 +109,29 @@ function work() {
                     </div>
                 </div>
 
+            </div>
+            <div style={{backgroundColor: "rgb(241, 241, 241)",marginTop:"20px",marginBottom:"-70px"}} class="hide-on-mobile " >
+
+                    <div class=" p-4 text-center fw-bold" style={{fontSize:"30px"}}>
+                        Subscribe to get news and updates
+                    </div>
+                    <div class=" sm-flex pt-2 pb-4 text-center ">
+                        <input placeholder="Your Email Address " class="fs-2 mt-1 mr-2" style={{width:"30%",height:"35px",textSize:"15px",backgroundColor: "#FCFAE8",border:"none",borderRadius:"5px"}}></input>
+                        <button class="btn  ml-4 "style={{backgroundColor: "#F2E88D",borderRadius:"5px"}} >Subscribe</button>
+                    </div>
+            </div>
+            <div style={{backgroundColor: "rgb(241, 241, 241)",marginBottom:"20px",marginBottom:"-40px"}} class="show-on-mobile " >
+
+                    <div class=" p-4 text-center fw-bold" style={{fontSize:"30px"}}>
+                        Subscribe to get news and updates
+                    </div>
+                    <div class=" sm-flex  pb-3 text-center ">
+                        <input placeholder="Your Email Address " class="fs-2 mt-1" style={{width:"80%",height:"35px",textSize:"15px",backgroundColor: "#FCFAE8",border:"none",borderRadius:"5px"}}></input>
+                    </div>
+                    <div class=" sm-flex pb-4 text-center ">
+                        <button class="btn  ml-4 "style={{backgroundColor: "#F2E88D",borderRadius:"5px"}} >Subscribe</button>
+
+                    </div>
             </div>
 
         </div>
