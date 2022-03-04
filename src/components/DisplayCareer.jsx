9f33e0053001c481ce7c1b1b8b2f9e245e8fedee
mishrashree18 <mishrashree18@gmail.com>
2022-03-04 16:18:55 +0530

    Display Career Page

--- src/components/DisplayCareer.jsx
+++ src/components/DisplayCareer.jsx
@@ -4,10 +4,22 @@ function DisplayCareer() {
   return (
     <section>
         <Navbar/>
-        <div style={{marginTop:"100px", marginLeft:"40px"}}>
+        <div style={{background:"#E6E6E6", height:"1000px"}}>
+        <div classname="align-items-center">
         <h1>Business Systems</h1>
+        <div class="box flex column">
+        <div class="dis">
+          <h3>Business Systems Analyst - Financial Systems</h3>
+          <p>Systems team to help expand and maintain our Order to Cash applications and its ecosystem. Your role will be to help implement, administer, and optimize our business systems and assist with business process </p>
+          <h5>Remote / Office </h5>
+        </div>
         <div>
-            
+          <div style={{display:"flex", flexDirection:"column"}}>
+            <p style={{color:"#4DFF0F"}}>Paid</p>
+            <p style={{color:"red"}}>Gratis</p>
+          </div>
+        </div>
+        </div>
         </div>
         </div>
     </section>)
