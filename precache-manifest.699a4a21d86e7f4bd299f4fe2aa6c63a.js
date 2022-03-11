fd6c6e709e6b089f4eda06d4818375fffe31bcd1
Arya Soni <aryasoni98@gmail.com>
2022-03-11 09:29:37 +0530

    Updates

---
+++ precache-manifest.699a4a21d86e7f4bd299f4fe2aa6c63a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "970d8840fea92f3c1e9d",
+    "url": "/static/js/main.970d8840.chunk.js"
+  },
+  {
+    "revision": "e1518f66cfe9b5998427",
+    "url": "/static/js/1.e1518f66.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c129c5475ced275fbea87e84e89cf235",
+    "url": "/index.html"
+  }
+];
