b8542eb635525cf4099e4c5762d7702b12758f5b
Soumali <gorai.soumali@gmail.com>
2022-03-23 14:21:04 +0530

    build

---
+++ build/precache-manifest.c4237b410f15bde0b16f243dad2b588b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3d55e8ede89e4c9160a",
+    "url": "/static/css/main.ac284dac.chunk.css"
+  },
+  {
+    "revision": "c3d55e8ede89e4c9160a",
+    "url": "/static/js/main.c3d55e8e.chunk.js"
+  },
+  {
+    "revision": "2a43fef87bd470b0aded",
+    "url": "/static/css/1.6222d49c.chunk.css"
+  },
+  {
+    "revision": "2a43fef87bd470b0aded",
+    "url": "/static/js/1.2a43fef8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff732bc2160d149a69558df52dea583e",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85446e48dfb4d6f66bc4bff801939857.js"
+  "/precache-manifest.c4237b410f15bde0b16f243dad2b588b.js"
 );
 
 workbox.clientsClaim();
